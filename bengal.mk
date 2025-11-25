03153a9604e05830cceac09ba4bdd8277eabd958
Daru <mica.tui@proton.me>
2025-11-25 13:36:42 +0200

    fix: replace 'android.hardware.gnss-aidl-impl-qti' with updated versions

--- bengal.mk
+++ bengal.mk
@@ -145,8 +145,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss-aidl-impl-qti \
-    android.hardware.gnss-aidl-service-qti \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti
     gnss@2.0-base.policy \
     gnss@2.0-xtra-daemon.policy \
     gnss@2.0-xtwifi-client.policy \
