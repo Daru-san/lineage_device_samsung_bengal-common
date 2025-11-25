465c67c0da49764d6e57967bd6ba62d2b4f558fe
Daru <mica.tui@proton.me>
2025-11-25 12:51:57 +0200

    fix: remove 'vendor/qcom/opensource/usb/etc' from soong product namespaces
    This causes conflicts when building the ROM.
    Furthermore, it is not necessary as the vendor repository already
    provides samsung-customized versions of the files in that folder.

--- bengal.mk
+++ bengal.mk
@@ -362,8 +362,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/samsung \
-    vendor/qcom/opensource/usb/etc
+    hardware/samsung
 
 # Telephony
 #PRODUCT_PACKAGES += \
