ac5fa3f45ba232a80181ceb49ec20998b56eb5b9
Daru <mica.tui@proton.me>
2025-11-25 22:26:03 +0200

    fix: add missing kernel cmdline flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,7 +50,7 @@ QCOM_BT_USE_BTNV := true
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on bootconfig loop.max_part=7
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on bootconfig loop.max_part=7
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_IMAGE_NAME := Image
